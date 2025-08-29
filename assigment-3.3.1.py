83566a262a2891275ab98e6c6d7a09329ae3100e
KRISHNKANT11 <pariharram12246@gmail.com>
2025-08-29 19:09:49 +0530

    Add files via upload

---
+++ assigment-3.3.1.py
@@ -0,0 +1,8 @@
+import math
+
+num = int(input("Enter a number: "))
+
+print("Square root :" , math.sqrt(num))
+print("logarithm :" , math.log(num))
+angle = math.pi / 2
+print("Sine :" , math.sin(angle))
